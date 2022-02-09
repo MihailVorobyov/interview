6aeb19b74700c402a3704a418fd82ba305e0e4a5
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-02-09 21:17:32 +0300

    Создан модуль 7 занятия. Создана сущность Student

---
+++ lesson-07-spring/src/main/java/ru/vorobev/spring/entities/Student.java
@@ -0,0 +1,24 @@
+package ru.vorobev.spring.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_id")
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "age")
+	private int age;
+	
+}
